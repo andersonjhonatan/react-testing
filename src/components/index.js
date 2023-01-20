d1ce6a3dd76bb0f12bc9111738080b6f3240818b
Anderson <105141966+andersonjhonatan@users.noreply.github.com>
2023-01-20 14:21:28 -0300

    testando ele

--- src/components/index.js
+++ src/components/index.js
@@ -3,3 +3,4 @@ export { default as FavoriteInput } from './FavoriteInput';
 export { default as Pokemon } from './Pokemon';
 export { default as PokemonButtonsPanel } from './PokemonButtonsPanel';
 export { default as PokemonData } from './PokemonData';
+//--
