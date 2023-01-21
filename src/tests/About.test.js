5453612beb056b501d8041f3738f99724595f4f3
Anderson <105141966+andersonjhonatan@users.noreply.github.com>
2023-01-21 00:08:28 -0300

    3 - requisito

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -4,7 +4,7 @@ import About from '../pages/About';
 describe('Teste se a página contém as informações sobre a Pokédex', () => {
   it('É exibido na tela um h2 com texto About Pokédex', () => {
     const { getByRole } = render(<About />);
-    expect(getByRole('heading', { name: 'About Pokédex' })).toBeInTheDocument();
+    expect(getByRole('heading', { level: 2, name: 'About Pokédex' })).toBeInTheDocument();
   });
 
   it('Teste se a página contém a seguinte imagem de uma Pokédex', () => {
--- src/tests/FavoritePokemon.test.js
+++ src/tests/FavoritePokemon.test.js
@@ -1,1 +1,20 @@
-test('', () => {});
+import { render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import App from '../App';
+import Favorite from '../pages/FavoritePokemon';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Ao favoritar a partir da página de detalhes', () => {
+  test('Teste se é exibida na tela a mensagem `No favorite pokemon found`, caso a pessoa não tenha Pokémon favoritos', () => {
+    const { getByText } = render(<Favorite />);
+    expect(getByText('No favorite Pokémon found')).toBeInTheDocument();
+  });
+
+  test('Teste se apenas são exibidos os Pokémon favoritados', () => {
+    const { getByText } = renderWithRouter(<App />);
+    userEvent.click(getByText(/more details/i));
+    userEvent.click(getByText(/pokémon favoritado?/i));
+    userEvent.click(getByText(/Favorite Pokémon/i));
+    expect(getByText(/pikachu/i)).toBeInTheDocument();
+  });
+});
