b25b0ec1a5e4fa1f184a322ec0c39032e30f6fad
Anderson <105141966+andersonjhonatan@users.noreply.github.com>
2023-01-21 01:26:48 -0300

    4 - requisito

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,13 @@
-test('', () => {});
+import { render } from '@testing-library/react';
+import NotFound from '../pages/NotFound';
+
+describe('Será avaliado se o arquivo teste NotFound.test.js contempla 100% dos casos de uso criados pelo Stryker', () => {
+  it('É exibido na tela um h2 com o texto Page requested not found', () => {
+    const { getByRole } = render(<NotFound />);
+    expect(getByRole('heading', { level: 2, name: /Page requested not found/i })).toBeInTheDocument();
+  });
+  it('Existe uma imagem com o src https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif', () => {
+    const { getByAltText } = render(<NotFound />);
+    expect(getByAltText(/Pikachu crying because the page requested was not found/i)).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
